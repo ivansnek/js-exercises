7c97d15e8659191cc33e870c3f490fb24914a84c
Ivan Eusebio <ieusebio@itexico.net>
2018-01-10 11:24:52 -0600

    Firs commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Basic Javascript Exercises
+
+## 1.- RFC CALCULATOR
+
+ * For this excecise we need to make a function which calculates the RFC for a given Person.
+ * This are the rules to generate RFC:
+
+ * PERSONAS FISICAS:
+  https://solucionfactible.com/sfic/capitulos/timbrado/rfc-persona-fisica.jsp
+ * PERSONAS MORALES:
+  https://solucionfactible.com/sfic/capitulos/timbrado/rfc-persona-moral.jsp
+
+  NOTES:
+
+  - For this excersice the `Homoclave` is going to be calculate randomly as a 3 alpha numeric string.
+  http://www.unionguanajuato.mx/articulo/2017/07/10/gobierno/cual-es-la-homoclave-de-mi-rfc
+  - No validate bad words.
+  - For `Persona Moral` no validate rules: 10, 11, 12.
+
---
+++ rfc-generator.js
@@ -0,0 +1,25 @@
+/**
+ * Feel free to edit this file with your code or use any number of methods or classes.
+*/
+
+const PERSONS = [
+  { names: 'Juan Antonio' , lastnames: 'Fernandez Muñoz', birthdate: new Date(1979,12,1), type: 'fisica' },
+  { names: 'Jose Carlos' , lastnames: 'Hernandez Perez', birthdate: new Date(1979,12,1), type: 'fisica' },
+  { names: 'Brenda Liliana' , lastnames: 'Gutierrez Velasco', birthdate: new Date(1979,12,1), type: 'fisica' },
+  { names: 'Maria Teresa' , lastnames: 'Herrera Torres', birthdate: new Date(1979,12,1), type: 'fisica' },
+  { names: 'Fernando' , lastnames: 'Perez Dieguez', birthdate: new Date(1979,12,1), type: 'fisica' },
+  { names: 'Luis Alberto' , lastnames: 'Elizondo Obrador', birthdate: new Date(1979,12,1), type: 'fisica' },
+  { names: 'Elvia' , lastnames: 'Jimenez Sanchez', birthdate: new Date(1979,12,1), type: 'fisica' },
+  { names: 'Pedro Joaquin' , lastnames: 'Alvarado Obregon', birthdate: new Date(1979,12,1), type: 'fisica' },
+
+  { name: 'Sonora Industrial Azucarera, S. de. R.L.	' , foundation: new Date(2012,9,13), type: 'moral' },
+  { name: 'Internacional Turística Flacón, S.A.' , foundation: new Date(1979,2,12), type: 'moral' },
+  { name: 'Candados, Llaves y Cerraduras, S.A.' , foundation: new Date(1999,1,11), type: 'moral' },
+  { name: 'H. Prieto y Martínez, S. de R.L.' , foundation: new Date(2017,3,5), type: 'moral' },
+  { name: 'Fonograbaciones Cinelandia, S. de R.L.' , foundation: new Date(2013,8,13), type: 'moral' },
+  { name: 'Arsuyama, S.A.' , foundation: new Date(2014,7,2), type: 'moral' },
+  { name: 'Al, S.A.' , foundation: new Date(1989,7,4), type: 'moral' },
+  { name: 'Los Viajes Internacionales de Marco Polo, S.A.' , foundation: new Date(1989,7,4), type: 'moral' }
+];
+
+// YOUR CODE
